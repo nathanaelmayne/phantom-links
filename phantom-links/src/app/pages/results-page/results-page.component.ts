f0e971dd55aaebddb2a6b15d281d25f86cf72992
Nathanael Mayne <nathanaelmayne@gmail.com>
2022-10-24 10:02:23 +0100

    Improved results page

--- phantom-links/src/app/pages/results-page/results-page.component.ts
+++ phantom-links/src/app/pages/results-page/results-page.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Bookmark } from 'src/app/models/bookmark.model';
+import { BookmarkService } from 'src/app/services/bookmark.service';
 
 @Component({
   selector: 'app-results-page',
   templateUrl: './results-page.component.html',
-  styleUrls: ['./results-page.component.scss']
+  styleUrls: ['./results-page.component.scss'],
 })
 export class ResultsPageComponent implements OnInit {
+  bookmarkId: string | undefined;
+  bookmark: Bookmark | undefined;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private bookmarkService: BookmarkService,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.route.queryParams
+      .subscribe((params) => {
+        this.bookmarkId = params['id'];
+        this.getBookmark();
+      });
   }
 
+  getBookmark(): void {
+    if (!this.bookmarkId) {
+      return;
+    }
+
+    this.bookmark = this.bookmarkService
+      .get(this.bookmarkId);
+  }
+
+  handleBackClicked(): void {
+    this.router.navigate(['']);
+  }
 }
