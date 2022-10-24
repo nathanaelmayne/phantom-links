94bcd5abb9d2b86fa4b2b69a0725644e88146b58
Nathanael Mayne <nathanaelmayne@gmail.com>
2022-10-24 08:10:56 +0100

    Created overview and results pages

--- phantom-links/src/app/app-routing.module.ts
+++ phantom-links/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { OverviewPageComponent } from './pages/overview-page/overview-page.component';
+import { ResultsPageComponent } from './pages/results-page/results-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: OverviewPageComponent },
+  { path: 'Results', component: ResultsPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- phantom-links/src/app/app.module.ts
+++ phantom-links/src/app/app.module.ts
@@ -3,10 +3,14 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import { OverviewPageComponent } from './pages/overview-page/overview-page.component';
+import { ResultsPageComponent } from './pages/results-page/results-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    OverviewPageComponent,
+    ResultsPageComponent,
   ],
   imports: [
     BrowserModule,
